6295cfa7b9b95140e30997f5d33cf56a76ae1311
Alex Shemyakin <shemyakin@yoctocode.com>
2020-04-21 20:30:07 +0300

    update namespace

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ansible-galaxy collection build
 
-ansible-galaxy collection install galaxy-dev-sample-test_mod-1.0.2.tar.gz
+ansible-galaxy collection install ashemyakin-test_mod-1.0.2.tar.gz
 
 ansible-playbook sample.yml -vvv
--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,3 @@
 ansible-galaxy collection build
-ansible-galaxy collection install galaxydevsample-test_mod-1.0.0.tar.gz -f -p  ~/collections
+ansible-galaxy collection install ashemyakin-test_mod-1.0.0.tar.gz -f -p  ~/collections
 ansible-playbook sample.yml -vvvvv
